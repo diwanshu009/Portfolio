c62a30996256b1b2eadea773492d6a4692a759b5
diwanshu009 <goddiwanshu@gmail.com>
2025-04-21 23:20:47 +0530

    starting a new design for portfolio website

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,35 @@
-import styled, { ThemeProvider } from "styled-components";
-import { darkTheme } from "./utils/Themes";
-import Navbar from "./components/Navbar";
-import { BrowserRouter } from "react-router-dom";
-import Hero from "./components/sections/Hero";
-import Skills from "./components/Sections/Skills";
-import Experience from "./components/Sections/Experience";
-import Education from "./components/Sections/Education";
-import StartCanvas from "./components/Canvas/Stars";
-import Projects from "./components/Sections/Projects";
-import Contact from "./components/Sections/Contact"
-import Footer from "./components/Sections/Footer";
-
-const Body = styled.div`
-  background-color: ${({ theme }) => theme.bg};
-  width: 100%;
-  overflow-x: hidden;
-  position: relative;
-`;
-
-const Wrapper = styled.div`
-  padding-bottom: 100px;
-  background: linear-gradient(
-      38.73deg,
-      rgba(204, 0, 187, 0.15) 0%,
-      rgba(201, 32, 184, 0) 50%
-    ),
-    linear-gradient(
-      141.27deg,
-      rgba(0, 70, 209, 0) 50%,
-      rgba(0, 70, 209, 0.15) 100%
-    );
-  width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
-`;
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <ThemeProvider theme={darkTheme}>
-      <BrowserRouter>
-        <Navbar />
-        <Body>
-          <StartCanvas />
-          <div>
-            <Hero />
-            <Wrapper>
-              <Skills />
-              <Experience />
-            </Wrapper>
-            <Projects />
-            <Wrapper>
-              <Education />
-              <Contact/>
-            </Wrapper>
-            <Footer />
-          </div>
-        </Body>
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
